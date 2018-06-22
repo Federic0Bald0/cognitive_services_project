f751f05046f3da969f99ab8f902ec82079f4219f
Riccardo11 <minato.riccardo@gmail.com>
2018-06-22 18:33:56 +0200

    Add snippet to upload csv

--- Dataset/datastore.py
+++ Dataset/datastore.py
@@ -1,10 +1,24 @@
 from google.cloud import datastore
+import csv
 
 client = datastore.Client.from_service_account_json(
                         './credentials.json')
 
+CSV = 'dataset_test.csv'
 
-def add_book(book_id, title, author, image, editor=None):
+
+def add_csv():
+    with open(CSV) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader, None)
+        for (i,row) in enumerate(reader):
+            # i+1 because id must not be 0
+            add_book(i+1, row[0], row[1], row[3],
+                editor=row[2], reviews=row[4])            
+
+
+def add_book(book_id, title, author, image, editor=None,
+             reviews=None, descriptions=None):
 
     key = client.key('Book', book_id)
     book = datastore.Entity(
@@ -15,7 +29,8 @@ def add_book(book_id, title, author, image, editor=None):
         'title': title,
         'author': author,
         'image': image,
-        'editor': editor
+        'editor': editor,
+        'descriptions' : descriptions
     })
 
     client.put(book)
@@ -46,16 +61,18 @@ def get_some_books(cursor=None):
     return books, next_cursor
 
 
+add_csv()
+print list_books()
 
-book_key = add_book(3,
-                    title="Harry Potter e i doni della morte",
-                    author="J. K. Rowling",
-                    image="this is a placeholder",
-                    )
+# book_key = add_book(3,
+#                     title="Harry Potter e i doni della morte",
+#                     author="J. K. Rowling",
+#                     image="this is a placeholder",
+#                     )
 
-books, next_cursor = get_some_books()
-print books
-books, _ = get_some_books(next_cursor)
-print books
+# books, next_cursor = get_some_books()
+# print books
+# books, _ = get_some_books(next_cursor)
+# print books
 
 # delete_book(book_key)
