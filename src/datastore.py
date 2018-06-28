3b65e556542de8b747ff3095c9cacd892aebb7bf
Federico Baldo <federico@Federicos-MacBook-Pro.local>
2018-06-28 17:54:36 +0200

    fixed datastore script - added all elements of dataset to datastore

--- src/datastore.py
+++ src/datastore.py
@@ -1,11 +1,12 @@
 import csv
+import ast
 from google.cloud import datastore
 from difflib import SequenceMatcher
 
 client = datastore.Client.from_service_account_json(
                         'credentials_datastore.json')
 
-CSV = 'datastore/dataset_test.csv'
+CSV = 'dataset/dataset.csv'
 
 
 def add_csv():
@@ -14,28 +15,50 @@ def add_csv():
         header = next(reader, None)
         for (i, row) in enumerate(reader):
             # i+1 because id must not be 0
-            add_book(i+1, row[0], row[1], row[3],
-                     editor=row[2], reviews=row[4])
+            reviews = ast.literal_eval(row[6])
+            add_book(i+1, row[0], row[1], row[3], row[4],
+                     row[5], reviews, row[2])
 
 
-def add_book(book_id, title, author, image, editor=None,
-             reviews=None, descriptions=None):
+def add_book(book_id, title, author, image, rating, price,
+             reviews, editor, descriptions=None):
 
     key = client.key('Book', book_id)
     book = datastore.Entity(
         key, exclude_from_indexes=['description'])
-    print book
+    # print book
 
     book.update({
         'title': title,
         'author': author,
         'image': image,
         'editor': editor,
+        'rating': rating,
+        'price': price,
         'descriptions': descriptions
     })
+    for review in reviews:
+        # only reviews with less then 1500 char are allowed
+        if len(review) < 1500:
+            add_review(key, review)
 
     client.put(book)
     return book.key
+
+
+def add_review(book_key, review):
+
+    key = client.key('Review')
+    rev = datastore.Entity(
+        key, exclude_from_indexes=['description'])
+
+    rev.update({
+        'book_key': book_key,
+        'review': review
+    })
+
+    client.put(rev)
+    return rev.key
 
 
 def delete_book(book_key):
@@ -84,9 +107,9 @@ def get_strings_diff(string1, string2):
     return SequenceMatcher(None, string1, string2).ratio()
 
 if __name__ == '__main__':
-    # add_csv()
+    add_csv()
     # print list_books()
-    print(search_book("Io, te e il ,Marzia Sici"))
+    # print(search_book("Io, te e il ,Marzia Sici"))
 
 
 # book_key = add_book(3,
